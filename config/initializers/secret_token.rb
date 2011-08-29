4a6e9ca4c7f9fdcb9d3f47033c871b4ee66e0b30
skeller1 <MiStK@gmx.de>
2011-08-29 19:09:01 +0200

    initial rails 3.1 standard app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhoenixR::Application.config.secret_token = '5cc12a2a4b06197c76746fae4df4beb63fe8ddf2359b2eda79f17dd0046e0060436bdf492ad7ef889a5956a4a3108eb480c4be9ab0a1be4974a02923e2acdb8d'
