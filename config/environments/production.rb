a0b7d5e1cc6340302f16fbdc427f3b1eb5436fa7
Stephan Keller <MiStK@gmx.de>
2011-10-04 15:48:10 +0200

    new production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ PhoenixR::Application.configure do
   config.assets.compress = false
 
     # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
